9825404b13a33d96de03e750689c463ad5aa2318
Tapan Parikh <tparikh@gmail.com>
2022-06-28 21:48:54 -0400

    change upload size limit

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,6 +5,17 @@ module.exports = [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
