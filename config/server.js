d6022347f61fad003e4111999883c6f393197762
Tapan Parikh <tparikh@gmail.com>
2022-08-16 18:01:16 -0400

    Delete server.js

--- config/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: ['iSLUu474x26TEqHMvvRE7w==', 'tb/QZ1CGTkDDEQSM62XKrg=='],
-  },
-});
