df342b1859d050d943dfa5d2c66ad1ab8ad42485
tap2k <tparikh@gmail.com>
2022-09-06 13:38:56 +0000

    unify config files

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: 'vl2rR+G9lEi8vl6J23zy3A==',
+    secret: env('ADMIN_JWT_SECRET', 'vl2rR+G9lEi8vl6J23zy3A=='),
   },
   apiToken: {
-    salt: 'PhbBQsqyHvsr0hZHjZy3fA==',
+    salt: env('API_TOKEN_SALT', 'PhbBQsqyHvsr0hZHjZy3fA=='),
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://mvc.represent.org/strapi'),
   app: {
-    keys: ['iSLUu474x26TEqHMvvRE7w==', 'tb/QZ1CGTkDDEQSM62XKrg=='],
+    keys: env.array('APP_KEYS', ['iSLUu474x26TEqHMvvRE7w==', 'tb/QZ1CGTkDDEQSM62XKrg==']),
   },
 });
