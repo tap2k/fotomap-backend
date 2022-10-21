98e012c55f79620c3c97e432e316024b81467d0d
Tapan Parikh <tparikh@gmail.com>
2022-10-21 09:02:39 -0400

    revert

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://mvc.represent.org/strapi'),
+  url: env('PUBLIC_URL', 'https://mvc.represent.org/strapi'),
   app: {
     keys: env.array('APP_KEYS', ['iSLUu474x26TEqHMvvRE7w==', 'tb/QZ1CGTkDDEQSM62XKrg==']),
   },
