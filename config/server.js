690862017e71dbc1a0d4b5b938a3c55232c65134
Tapan Parikh <tparikh@gmail.com>
2023-02-24 16:55:59 -0500

    remove telemetry

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://192.168.50.17:1337'),
   app: {
     keys: env.array('APP_KEYS', ['iSLUu474x26TEqHMvvRE7w==', 'tb/QZ1CGTkDDEQSM62XKrg==']),
   },
