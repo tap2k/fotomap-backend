43b5252947621f4598c5622b357a140009681621
Tapan Parikh <tparikh@gmail.com>
2023-03-26 08:46:48 -0400

    tighten up

--- src/api/channel/controllers/channel.js
+++ src/api/channel/controllers/channel.js
@@ -29,7 +29,7 @@ module.exports = createCoreController('api::channel.channel', ({ strapi }) =>  (
     async getMyChannels(ctx) {
         const channels = await strapi.db.query('api::channel.channel').findMany({
             select: ['uniqueID', 'name', 'lat', 'long', 'zoom'],
-            where: { owner: ctx.state.user.id },
+            where: { $and: [{ owner: ctx.state.user.id }, { parent: {$neq: null} }] }
         });
         return channels;
     },
