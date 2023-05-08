a8e39cb32b9af09ff70069900af274dc60953281
Tapan Parikh <tparikh@gmail.com>
2023-05-07 20:44:03 -0400

    add config

---
+++ config/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env('SMTP_PORT', 465),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'tapan@represent.org',
+        defaultReplyTo: 'tapan@represent.org',
+      },
+    },
+  },
+});
