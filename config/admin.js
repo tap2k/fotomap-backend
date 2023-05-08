c050b708f0f55f5ff88a95394d9c092038630d87
Tapan Parikh <tparikh@gmail.com>
2023-05-08 08:48:55 -0400

    Delete config directory

--- config/admin.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'vl2rR+G9lEi8vl6J23zy3A=='),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT', 'PhbBQsqyHvsr0hZHjZy3fA=='),
-  },
-});
