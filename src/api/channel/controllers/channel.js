3fcb94a167b222da381896cd604260e55e5e4362
Tapan Parikh <tparikh@gmail.com>
2023-05-16 00:00:19 -0400

    dont allow reparenting

--- src/api/channel/controllers/channel.js
+++ src/api/channel/controllers/channel.js
@@ -295,6 +295,7 @@ module.exports = createCoreController('api::channel.channel', ({ strapi }) =>  (
         if (!channel) 
             return ctx.badRequest('No such channel or you are not allowed to edit: ' + ctx.request.body.uniqueID);
 
+        // TODO: Dont allow reparent?
         if (ctx.request.body.parent == channel.id)
             ctx.request.body.parent = null;
         else
