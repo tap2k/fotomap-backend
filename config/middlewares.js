159257e8d2c7e6c87dd96fa852936c3dcde03323
Tapan Parikh <tparikh@gmail.com>
2024-11-19 14:07:38 -0500

    ux4me integration

---
+++ config/middlewares.js
@@ -0,0 +1,106 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "1gb", // modify form body
+      jsonLimit: "1gb", // modify JSON body
+      textLimit: "1gb", // modify text body
+      formidable: {
+        maxFileSize: 1000 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  'global::authenticate'
+];
+
+module.exports = [
+  'strapi::errors',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            '*.nyc3.digitaloceanspaces.com',
+            'nyc3.digitaloceanspaces.com',
+            's3.us-east-005.backblazeb2.com',
+            '*.s3.us-east-005.backblazeb2.com',
+            'mvcdevcdn.represent.org',
+            'mvcprodcdn.represent.org',
+            'supabase.co', 
+            '*.supabase.co'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            '*.nyc3.digitaloceanspaces.com',
+            'nyc3.digitaloceanspaces.com',
+            's3.us-east-005.backblazeb2.com',
+            '*.s3.us-east-005.backblazeb2.com',
+            'mvcdevcdn.represent.org',
+            'mvcprodcdn.represent.org',
+            'supabase.co', 
+            '*.supabase.co'
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // Replace the simple 'strapi::cors' with this configuration
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      //origin: ['*'],
+      origin: ['https://mvcdev.represent.org', 'https://expressdev.represent.org', 'https://mvcprod.represent.org', 'https://mvc.represent.org', 'https://express.represent.org', 'https://bihar.represent.org', 'https://gujarat.represent.org', 'https://express.maustro.com', 'https://express.ux4.me', 'https://expressdev.ux4.me'], // Add your frontend URLs here
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "1gb",
+      jsonLimit: "1gb",
+      textLimit: "1gb",
+      formidable: {
+        maxFileSize: 1000 * 1024 * 1024,
+      },
+    },
+  },
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  'global::authenticate'
+];
+
