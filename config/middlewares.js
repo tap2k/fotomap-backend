0b821afaa7277aa2e9fcc15b35ecee7fdbee9e57
Tapan Parikh <tparikh@gmail.com>
2024-11-22 13:32:23 -0500

    fix configs

--- config/middlewares.js
+++ config/middlewares.js
@@ -33,7 +33,7 @@ module.exports = [
             'supabase.co', 
             '*.supabase.co'
           ],
-          upgradeInsecureRequests: true,
+          upgradeInsecureRequests: null,
         },
       },
     },
